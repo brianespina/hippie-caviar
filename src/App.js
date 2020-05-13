8d7fb2a7f01fac100026be52b8c26107d7a727b9
Brian Espina <espinabrian@gmail.com>
2020-05-13 18:18:20 +0800

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './default.scss'
 import Header from './components/Header'
 import Homepage from './components/pages/Homepage'
+import Registration from './components/pages/Registration'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="main">
-        <Homepage />
+        <Switch>
+          <Route exact path="/" component={Homepage}/>
+          <Route exact path="/registration" component={Registration}/>
+        </Switch>
       </div>
     </div>
   );
---
+++ src/components/pages/Registration/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.scss'
+
+const Registration = props => {
+    return(
+        <div>
+            <h1>Registration</h1>
+        </div>
+    )
+}
+
+export default Registration
