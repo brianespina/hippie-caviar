ae556213721ddb0e8275e68981201fe33432b132
Brian Espina <espinabrian@gmail.com>
2020-06-15 22:16:02 +0800

    moved shop data and directory data to redux

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -1,63 +1,27 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { selectDirectorySections } from '../../redux/directory/directory.selector'
+
 import MenuItem from './../menu-item/menu-item.component'
 import './directory.styles.scss'
 
-class Directory extends Component{
-    constructor(props){
-        super(props)
-
-        this.state = {
-            section: [
-                {
-                  title: 'hats',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1,
-                  linkUrl: 'hats'
-                },
-                {
-                  title: 'jackets',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2,
-                  linkUrl: 'jackets'
-                },
-                {
-                  title: 'sneakers',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3,
-                  linkUrl: 'sneakers'
-                },
-                {
-                  title: 'womens',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  size: 'large',
-                  id: 4,
-                  linkUrl: 'womens'
-                },
-                {
-                  title: 'mens',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  size: 'large',
-                  id: 5,
-                  linkUrl: 'mens'
-                }
-            ]
-        }
-    }
-
-    render(){
-        const menuitems = this.state.section.map(item => (
-            <MenuItem props={item} />
-        ))
-        return(
-            <div className="directory-menu">
-                { 
-                    this.state.section.map(({ id, ...other}) => (
-                        <MenuItem key={id} {...other}/>
-                    ))  
-                }
-            </div>
-        )
-    }
+function Directory ({ section }) {
+  const menuitems = section.map(item => (
+      <MenuItem props={item} />
+  ))
+  return(
+      <div className="directory-menu">
+          { 
+              section.map(({ id, ...other}) => (
+                  <MenuItem key={id} {...other}/>
+              ))  
+          }
+      </div>
+  )
 }
 
-export default Directory
+const mapStateToProps = createStructuredSelector({
+  section: selectDirectorySections
+})
+export default connect(mapStateToProps)(Directory)
--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,30 +1,25 @@
-import React, { Component }from 'react'
+import React from 'react'
 import { withRouter } from 'react-router-dom'
-import { SHOP_DATA } from './shop.data'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { selectShopCollections } from '../../redux/shop/shop.selectors' 
 
 import CollectionPreview from '../../components/preview-collection/preview-collections.component'
 
-class ShopPage extends Component{
-    constructor(){
-        super()
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render(){
-        const { collections } = this.state
-
-        return(
-            <div className="shop-page">
-                {
-                    collections.map(({ id, ...other}) =>(
-                        <CollectionPreview key={id} {...other}/>
-                    ))
-                }
-            </div>
-        )
-    }
+function ShopPage ({ collections }) {
+    return(
+        <div className="shop-page">
+            {
+                collections.map(({ id, ...other}) =>(
+                    <CollectionPreview key={id} {...other}/>
+                ))
+            }
+        </div>
+    )
 }
 
-export default withRouter(ShopPage)
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopCollections
+})
+
+export default connect(mapStateToProps)(withRouter(ShopPage))
